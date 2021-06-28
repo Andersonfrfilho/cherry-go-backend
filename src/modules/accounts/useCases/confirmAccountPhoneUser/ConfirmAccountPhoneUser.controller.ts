91d68c8300664275f6ae2a7bfd3662cedc1ccd24
Anderson <andersonfrfilho@gmail.com>
2021-06-28 19:37:23 -0300

    :recycle: confirm phone account

--- src/modules/accounts/useCases/confirmAccountPhoneUser/ConfirmAccountPhoneUser.controller.ts
+++ src/modules/accounts/useCases/confirmAccountPhoneUser/ConfirmAccountPhoneUser.controller.ts
@@ -1,3 +1,20 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
-});
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { ConfirmAccountPhoneUserService } from "@modules/accounts/useCases/confirmAccountPhoneUser/ConfirmAccountPhoneUser.service";
+import { HttpSuccessCodeEnum } from "@shared/infra/http/enums";
+
+class ConfirmAccountPhoneUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+    const { code, token } = request.body;
+    const confirmAccountPhoneUserService = container.resolve(
+      ConfirmAccountPhoneUserService
+    );
+
+    await confirmAccountPhoneUserService.execute({ token, code, user_id: id });
+
+    return response.status(HttpSuccessCodeEnum.NO_CONTENT).send();
+  }
+}
+export { ConfirmAccountPhoneUserController };
